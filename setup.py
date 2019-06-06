726165eaefc9cca30999abf342421bf184877ef0
Reece Dunham <me@rdil.rocks>
2019-06-06 16:52:12 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='crime',
+    version='0.1',
+    description='FBI crime API wrapper',
+    packages=setuptools.find_packages(),
+    author='Cakebot Team',
+    author_email='cakebot@rdil.rocks',
+    url='https://github.com/cakebotpro/crime'
+)
