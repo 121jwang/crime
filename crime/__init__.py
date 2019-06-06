db802d9d1b36fad2276a79c2cf7628ab162db985
Reece Dunham <me@rdil.rocks>
2019-06-06 16:49:19 -0400

    base code

---
+++ crime/__init__.py
@@ -0,0 +1,16 @@
+import urllib
+import json
+
+
+class Imp:
+    def __init__(self, key):
+        urllib.request.urlcleanup()
+        self.response = urllib.request.urlopen(
+            urllib.request.Request(
+                f"https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/arson/offense/national/count?api_key={key}"
+            )
+        )
+        self.obj = json.loads(self.response.read())
+
+    def results(self):
+        return self.obj['results']
